6baa11100b34d14d5ae8238cdef330b21db4cd68
Andrew Adams <aadamsdev@gmail.com>
2018-03-28 15:32:36 -0400

    working on login/registration

--- app/src/main/java/com/aadamsdev/communities/fragments/SplashFragment.java
+++ app/src/main/java/com/aadamsdev/communities/fragments/SplashFragment.java
@@ -11,6 +11,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
 import android.view.LayoutInflater;
@@ -30,6 +31,8 @@ import com.aadamsdev.communities.utils.PreferenceManager;
 
 import java.util.ArrayList;
 
+import butterknife.BindView;
+
 import static android.content.SharedPreferences.*;
 
 /**
@@ -37,18 +40,22 @@ import static android.content.SharedPreferences.*;
  */
 public class SplashFragment extends Fragment {
 
-    private View view;
-    private Context context;
-
     private PreferenceManager preferenceManager;
 
-    private ViewPager viewPager;
-    private SplashScreenPagerAdapter myViewPagerAdapter;
-    private LinearLayout dotsLayout;
-    private TextView[] dots;
-    private int[] layouts;
-    private Button btnNext;
+    @BindView(R.id.username_field)
+    EditText usernameField;
 
+    @BindView(R.id.password_field)
+    EditText passwordField;
+
+    @BindView(R.id.login_button)
+    Button loginButton;
+
+    @BindView(R.id.register_button)
+    Button registerButton;
+
+    @BindView(R.id.forgot_password_button)
+    Button forgotPasswordButton;
 
     public static SplashFragment newFragment() {
         return new SplashFragment();
@@ -58,134 +65,28 @@ public class SplashFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-//        ((AppCompatActivity) getActivity()).getSupportActionBar().hide();
-
-        context = getContext();
-
+        hideActionBar();
         // Checking for first time launch - before calling setContentView()
-        preferenceManager = PreferenceManager.getInstance(getContext());
-        //TODO: Remove comment to skip splash screen
-        if (!preferenceManager.isFirstTimeLaunch()) {
-            launchChatActivity();
-        } else {
-            preferenceManager.setFirstTimeLaunch(false);
-        }
+//        preferenceManager = PreferenceManager.getInstance(getActivity());
+//        //TODO: Remove comment to skip splash screen
+//        if (!preferenceManager.isFirstTimeLaunch()) {
+//            launchChatActivity();
+//        } else {
+//            preferenceManager.setFirstTimeLaunch(false);
+//        }
+
     }
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.splash_fragment, container, false);
-
-        if (Build.VERSION.SDK_INT >= 21) {
-            getActivity().getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-            Toast.makeText(getContext(), "Hiding action bar...", Toast.LENGTH_SHORT).show();
-        }
-
-        // TODO IMPLEMENT BUTTERKNIFE FOR VIEW BINDING
-        viewPager = (ViewPager) view.findViewById(R.id.view_pager);
-        dotsLayout = (LinearLayout) view.findViewById(R.id.layoutDots);
-        btnNext = (Button) view.findViewById(R.id.btn_next);
-
-        // layouts of all welcome sliders
-        // add few more layouts if you want
-        layouts = new int[]{
-                R.layout.splash_screen1,
-                R.layout.splash_screen2,
-                R.layout.splash_screen3};
-
-        // adding bottom dots
-        addBottomDots(0);
-
-        // making notification bar transparent
-        changeStatusBarColor();
-
-        myViewPagerAdapter = new SplashScreenPagerAdapter();
-        viewPager.setAdapter(myViewPagerAdapter);
-        viewPager.addOnPageChangeListener(viewPagerPageChangeListener);
-
-        btnNext.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // checking for last page
-                // if last page home screen will be launched
-                int current = getItem(+1);
-                if (current < layouts.length) {
-                    // move to next screen
-                    viewPager.setCurrentItem(current);
-                } else {
-                    EditText usernameTextView = (EditText) view.findViewById(R.id.username);
-                    String username = usernameTextView.getText().toString();
-
-                    preferenceManager.setUsername(username);
-
-                    //TODO: Probably need a loading dialog here...
-                    launchChatActivity();
-                }
-            }
-        });
+        View view = inflater.inflate(R.layout.splash_fragment, container, false);
 
         return view;
     }
 
-    private void addBottomDots(int currentPage) {
-        dots = new TextView[layouts.length];
-
-        int[] colorsActive = getResources().getIntArray(R.array.array_dot_active);
-        int[] colorsInactive = getResources().getIntArray(R.array.array_dot_inactive);
-
-        dotsLayout.removeAllViews();
-        for (int i = 0; i < dots.length; i++) {
-            dots[i] = new TextView(getContext());
-            dots[i].setText(Html.fromHtml("&#8226;"));
-            dots[i].setTextSize(35);
-            dots[i].setTextColor(colorsInactive[currentPage]);
-            dotsLayout.addView(dots[i]);
-        }
-
-        if (dots.length > 0) {
-            dots[currentPage].setTextColor(colorsActive[currentPage]);
-        }
-    }
-
-    private int getItem(int i) {
-        return viewPager.getCurrentItem() + i;
-    }
-
-    //  viewpager change listener
-    ViewPager.OnPageChangeListener viewPagerPageChangeListener = new ViewPager.OnPageChangeListener() {
-
-        @Override
-        public void onPageSelected(int position) {
-            addBottomDots(position);
-
-            // changing the next button text 'NEXT' / 'GOT IT'
-            if (position == layouts.length - 1) {
-                // last page. make button text to GOT IT
-                btnNext.setText(getString(R.string.start));
-            } else {
-                // still pages are left
-                btnNext.setText(getString(R.string.next));
-            }
-        }
-
-        @Override
-        public void onPageScrolled(int arg0, float arg1, int arg2) {
-
-        }
-
-        @Override
-        public void onPageScrollStateChanged(int arg0) {
-
-        }
-    };
-
-    /**
-     * Making notification bar transparent
-     */
-    private void changeStatusBarColor() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            Window window = getActivity().getWindow();
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.setStatusBarColor(Color.TRANSPARENT);
+    private void hideActionBar() {
+        ActionBar actionBar = ((AppCompatActivity) getActivity()).getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
         }
     }
 
@@ -193,39 +94,4 @@ public class SplashFragment extends Fragment {
         Intent intent = ChatActivity.newIntent(getContext());
         getActivity().startActivity(intent);
     }
-
-    private class SplashScreenPagerAdapter extends PagerAdapter {
-        private LayoutInflater layoutInflater;
-
-        public SplashScreenPagerAdapter() {
-        }
-
-        @Override
-        public Object instantiateItem(ViewGroup container, int position) {
-            layoutInflater = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-
-            View view = layoutInflater.inflate(layouts[position], container, false);
-            container.addView(view);
-
-            return view;
-        }
-
-        @Override
-        public int getCount() {
-            return layouts.length;
-        }
-
-        @Override
-        public boolean isViewFromObject(View view, Object obj) {
-            return view == obj;
-        }
-
-
-        @Override
-        public void destroyItem(ViewGroup container, int position, Object object) {
-            View view = (View) object;
-            container.removeView(view);
-        }
-    }
-
 }
